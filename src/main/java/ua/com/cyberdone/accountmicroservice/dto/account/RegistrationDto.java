904a363a1489fa7753797af46a03f0ac56a70dee
Ostap Komplikevych <ostap.ja@gmail.com>
2022-02-04 15:22:19 +0200

    [cdas-15] fix validation in RegistrationDto

--- src/main/java/ua/com/cyberdone/accountmicroservice/dto/account/RegistrationDto.java
+++ src/main/java/ua/com/cyberdone/accountmicroservice/dto/account/RegistrationDto.java
@@ -3,7 +3,9 @@ package ua.com.cyberdone.accountmicroservice.dto.account;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import ua.com.cyberdone.accountmicroservice.common.constant.Regex;
 
+import javax.validation.constraints.Pattern;
 import java.util.Set;
 
 @Data
@@ -20,10 +22,20 @@ import java.util.Set;
         "    ]\n" +
         "}")
 public class RegistrationDto {
+    @Pattern(regexp = Regex.EMAIL,
+            message = Regex.EMAIL_FAIL_MESSAGE)
     private String username;
+    @Pattern(regexp = Regex.PASSWORD,
+            message = Regex.PASSWORD_FAIL_MESSAGE)
     private String password;
+    @Pattern(regexp = Regex.FIRST_NAME,
+            message = Regex.FIRST_NAME_FAIL_MESSAGE)
     private String firstName;
+    @Pattern(regexp = Regex.LAST_NAME,
+            message = Regex.LAST_NAME_FAIL_MESSAGE)
     private String lastName;
+    @Pattern(regexp = Regex.PATRONYMIC,
+            message = Regex.PATRONYMIC_FAIL_MESSAGE)
     private String patronymic;
     private Set<String> roles;
 }
